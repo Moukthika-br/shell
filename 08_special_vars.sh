dbab8baf42709223f2fd294f3452003ee3e5f637
Moukthika-br <mothukurimoukthika@gmail.com>
2025-08-31 23:16:12 +0530

    hello

--- 08_special_vars.sh
+++ 08_special_vars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "ALL variable pass to the script : $@"
+echo "Script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
