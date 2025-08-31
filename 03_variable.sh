483dbde16acbc27e6ee8fa3ab0d12874b4305978
Moukthika-br <mothukurimoukthika@gmail.com>
2025-08-31 21:39:26 +0530

    hello people

---
+++ 03_variable.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+
+PERSON1 = moukthika
+PERSON2 = idiot
+
+echo "PERSON1: hi, how are you"
+echo "PERSON2: I am fine, thank you for asking"
+echo "PERSON1: please have some breakfast and go"
+echo "PERSON2: thank you , will do definitely"
