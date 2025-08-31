5a3998e3ccf3fb78191417204c4ed970f1682b1f
Moukthika-br <mothukurimoukthika@gmail.com>
2025-08-31 21:43:23 +0530

    hello

--- 03_variable.sh
+++ 03_variable.sh
@@ -3,7 +3,8 @@
 PERSON1=moukthika #no space must be give, it hits the error
 PERSON2=idiot
 
-echo "$PERSON1: hi, how are you"
-echo "$PERSON2: I am fine, thank you for asking"
-echo "$PERSON1: please have some breakfast and go"
-echo "$PERSON2: thank you , will do definitely"
+echo "$PERSON1:: hi, how are you"
+echo "$PERSON2:: I am fine, thank you for asking"
+echo "$PERSON1:: please have some breakfast and go"
+echo "$PERSON2:: thank you , will do definitely"
+echo "$PERSON2:: thank you , will do definitely"
