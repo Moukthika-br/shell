ca834d4dd0a13e8e1956e4f503c8bd31d4317cd6
Moukthika-br <mothukurimoukthika@gmail.com>
2025-08-31 19:31:45 +0530

    hello people

---
+++ 02_conversation.sh
@@ -0,0 +1,6 @@
+#! bin/bash
+
+echo "trump: hi, how are you"
+echo "musk: I am fine, thank you for asking"
+echo "trump: please have some breakfast and go"
+echo "musk: thank you , will do definitely"
