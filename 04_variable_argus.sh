7a6bde062781277e45a50f5cb7a807a22c542b06
Moukthika-br <mothukurimoukthika@gmail.com>
2025-08-31 21:50:18 +0530

    hello

---
+++ 04_variable_argus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 #no space must be give, it hits the error
+PERSON2=$2
+
+echo "$PERSON1:: hi, how are you"
+echo "$PERSON2:: I am fine, thank you for asking"
+echo "$PERSON1:: please have some breakfast and go"
+echo "$PERSON2:: thank you , will do definitely"
+echo "$PERSON2:: thank you , will do definitely"
